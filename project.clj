c97adba5facaed81bfb1c4c44bc47cf8117d7822
Paul Rutledge <rutledgepaulv@gmail.com>
2020-06-05 08:56:11 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rutledgepaulv/avow "0.1.0-SNAPSHOT"
+(defproject org.clojars.rutledgepaulv/avow "0.1.0"
 
   :description
   "An open definition of equality for more concise and robust tests."
